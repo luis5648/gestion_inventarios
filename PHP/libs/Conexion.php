c1a185bb7b435ecae7a4d9d8ab92a7ddba204276
Luis Román <porta5648@gmail.com>
2019-03-06 10:58:54 -0600

    Merge branch 'master' of C:\xampp\htdocs\inventario_ucta with conflicts.

--- PHP/libs/Conexion.php
+++ PHP/libs/Conexion.php
@@ -3,14 +3,14 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "inventario_ucta_t";
+$dbname = "inventario_ucta";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 $acentos = $conn->query("SET NAMES 'utf8'");
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
-exit();
+
 }
 
 
